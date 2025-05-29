7db8e6b6f59732004948487598eab9abf10860ab
k4sp3risme <145744192+spoooky762@users.noreply.github.com>
2025-05-30 05:00:05 +0800

    Update Info.java

--- src/burp/Info.java
+++ src/burp/Info.java
@@ -5,7 +5,11 @@ package burp;
 * @github https://github.com/bit4woo 
 * @version CreateTime：Jun 25, 2020 3:21:11 PM 
 */
-
+/** 
+* @translator Spoooky762
+* @github https://github.com/spoooky762 
+* @version CreateTime：May 29, 2025 5:00:00 AM 
+*/
 public class Info {
 	public static String ExtensionName = "reCAPTCHA";
 	public static String Version = bsh.This.class.getPackage().getImplementationVersion();
